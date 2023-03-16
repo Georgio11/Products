acecf45fc03856f46536079d514d0778d7a659da
Georgio11 <senchenkoegor@gmail.com>
2023-03-16 17:42:28 +0200

    add commit

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ let url = 'https://my-json-server.typicode.com/Georgio11/Products';
 xhr.open('GET', url + '/products');
 xhr.responseType = 'json';
 xhr.onload = function() {
-	let products = xhr.response;
+	let productsArray = xhr.response;
 	productsGrid.innerHTML = null;
-	products.forEach(p => {
+	productsArray.forEach(p => {
 		productsArray.push(p);
 		let pElem = document.createElement('div');
 		pElem.classList.add('product');
@@ -18,9 +18,47 @@ xhr.onload = function() {
 			<p class='product-price'><b>Price: </b>${p.price}</p>
 			<p class='product-description'><b>Description: </b>${p.description}</p>
 			<a href="profile.html?id=${p.author_id}">Seller profile</a>
-			<button>Buy</button>
+			<button onclick="addProductToCart(${p.id})">Buy</button>
 		`;
 		productsGrid.append(pElem);
 	});
 }
 xhr.send();
+
+let cartProd = document.getElementById('cart-products');
+
+let cart = [];
+
+function addProductToCart(id) {
+	let product = productsArray.find(function(p) {
+		return p.id == id;
+	})
+	cart.push(product);
+	drawCartProducts();
+}
+
+function drawCartProducts() {
+	if (cart.length == 0) return cartProd.innerHTML = 'Cart is empty';
+	cartProd.innerHTML = null;
+	let sum = 0;
+	cart.forEach(function(p) {
+		cartProd.innerHTML += `
+			<p><img src="${p.photo_url}">${p.name} |${p.price}</p>
+			<hr>
+		`;
+		sum += p.price;
+	});
+	cartProd.innerHTML += `
+		<p>Total price: ${sum}</p>
+		<button onclick="buyAll()">Buy all</button>
+	`
+}
+
+function buyAll() {
+	cart = [];
+	cartProd.innerHTML = 'Money was withdrawn from your credit card'
+}
+
+function openCart() {
+	cartProd.classList.toggle('hide');
+}
